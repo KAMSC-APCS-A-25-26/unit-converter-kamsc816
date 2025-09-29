8436c4b95d4272b136bfafb781182b610d27f169
kamsc816 <03samuel.miller@kamsc.org>
2025-09-29 13:46:07 -0400

    Update DistanceConverter.java

--- src/main/java/DistanceConverter.java
+++ src/main/java/DistanceConverter.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class DistanceConverter {
     public static void main(String[] args) {
-        // TODO: Implement distance converter
+        
         // Requirements:
         // - Menu with options: 1. Convert Miles to Kilometers, 2. Convert Kilometers to Miles, 3. Exit
         // - Loop until user chooses to exit
